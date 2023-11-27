778d79e595c0d909b1c187cdf5f9edbc1688f6dc
Andrew <andrewbrodriguez@gmail.com>
2023-11-27 13:34:28 -0500

    updates

--- backend/image_gen.py
+++ backend/image_gen.py
@@ -7,3 +7,16 @@ data = json.load(f)
 key = data["dalle"]
 
 
+import openai
+
+PROMPT = "An eco-friendly computer from the 90s in the style of vaporwave"
+
+openai.api_key = key 
+
+response = openai.Image.create(
+    prompt=PROMPT,
+    n=1,
+    size="256x256",
+)
+
+print(response["data"][0]["url"])
