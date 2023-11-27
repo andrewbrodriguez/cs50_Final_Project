56d690684ab196defa60035cd2abdd25951b2542
Andrew <andrewbrodriguez@gmail.com>
2023-11-27 16:12:14 -0500

    base level prompt gen

--- backend/prompt_gen.py
+++ backend/prompt_gen.py
@@ -1,1 +1,25 @@
-#gpt shit
+import json
+import openai
+
+f = open('secrets.json')
+
+data = json.load(f)
+
+key = data["gpt"]
+
+
+openai.api_key = key
+messages = [ {"role": "system", "content": 
+			"You are a intelligent assistant."} ] 
+while True: 
+	message = input("User : ") 
+	if message: 
+		messages.append( 
+			{"role": "user", "content": message}, 
+		) 
+		chat = openai.ChatCompletion.create( 
+			model="gpt-3.5-turbo", messages=messages 
+		) 
+	reply = chat.choices[0].message.content 
+	print(f"ChatGPT: {reply}") 
+	messages.append({"role": "assistant", "content": reply}) 
