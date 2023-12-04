3dc54a760f6748b440f3c8326db0f1d5d76a9139
Andrew <andrewbrodriguez@gmail.com>
2023-12-04 17:41:45 -0500

    finished gens

--- code/image_gen.py
+++ code/image_gen.py
@@ -1,46 +1,8 @@
 import json
 import os
 import openai 
-#import OpenAI
-
-#client = OpenAI()
 import requests
 
-def create_image(prompt):
-    # Open and read the API key from secrets.json
-    with open('secrets.json') as f:
-        data = json.load(f)
-
-    key = data["dalle"]
-    
-    output_directory = "images"
-
-    promptNumber = prompt[0]
-    prompt = prompt[1:]
-
-    description = prompt
-    style = "a high fantasy painting"
-
-    response = client.images.generate(prompt=description + " in the style of " + style,
-    n=1,
-    size="512x512")
-
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-
-    # Download and save the images
-    # data = response['data']
-    # link = data[0]
-    # link = link["url"]
-    link = response.data[0].url
-    image_data = requests.get(link).content
-    image_path = os.path.join(output_directory, f'image_{promptNumber}.png')
-
-    with open(image_path, 'wb') as image_file:
-        image_file.write(image_data)
-
-    return response
 
 
 def create_img(prompt):
@@ -88,5 +50,3 @@ def create_img(prompt):
         # If an error occurs, return an error message or handle it as needed
         return {"error": str(e)}
 
-
-print(create_img("a tree"))
--- code/prompt_gen.py
+++ code/prompt_gen.py
@@ -1,44 +1,5 @@
 import json
 import openai
-#from openai import OpenAI
-
-#client = OpenAI()
-
-from helpers import apology
-
-def create_prompts(blocks):
-
-	prompt_count = 0
-
-	f = open('secrets.json')
-	data = json.load(f)
-	key = data["gpt"]
-	
-
-	prompts = []
-
-	messages = [ {"role": "system", "content": 
-				"Your goal is to describe the following paragraph such that a painter can make an image in its likness. You must use descriptive words. Make sure that the wording of the prompt is not violent, or rude, or sinister. The wording of the prompt must adhere to the OpenAI Dalle2 input prompt regulations. Your response must be less than 1 sentences in length. Here is the paragraph: "} ] 
-
-	while len(blocks) > 0: 
-
-		messages.append( 
-			{"role": "user", "content": blocks[0]}, 
-		) 
-		chat = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages) 
-		reply = chat.choices[0].message.content 
-		prompt_count+=1
-		prompts.append(str(prompt_count) + reply)
-		messages.append({"role": "assistant", "content": reply}) 
-		
-		if len(blocks) <  2:
-			break
-
-		blocks.pop(0)
-		print("Prompt " + str(prompt_count) + " done!")
-	
-
-	return prompts
 
 def make_prompts(blocks):
 
@@ -77,6 +38,3 @@ def make_prompts(blocks):
 
 	return prompts
 
-b = ['a tree', 'a fish']
-
-print(make_prompts(b))
