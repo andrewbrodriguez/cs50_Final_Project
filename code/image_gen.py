81fcad83089f12dcb9d950716afac827c5c1eb31
Andrew <andrewbrodriguez@gmail.com>
2023-12-04 17:16:15 -0500

    test for maddie

--- code/image_gen.py
+++ code/image_gen.py
@@ -1,8 +1,9 @@
 import json
 import os
-from openai import OpenAI
+import openai 
+#import OpenAI
 
-client = OpenAI()
+#client = OpenAI()
 import requests
 
 def create_image(prompt):
@@ -41,3 +42,51 @@ def create_image(prompt):
 
     return response
 
+
+def create_img(prompt):
+
+    try:
+
+        prompt = prompt[0:399]
+        # Open and read the API key from secrets.json
+        with open('secrets.json') as f:
+            data = json.load(f)
+
+        key = data["dalle"]
+        openai.api_key = key
+        output_directory = "images"
+
+        promptNumber = prompt[0]
+        prompt = prompt[1:]
+
+        description = prompt
+        style = "a high fantasy painting"
+        
+        response = openai.Image.create(
+            prompt=description + " in the style of " + style,
+            n=1,
+            size="512x512"
+        )
+
+        # Create the output directory if it doesn't exist
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        # Download and save the images
+        data = response['data']
+        link = data[0]
+        link = link["url"]
+        image_data = requests.get(link).content
+        image_path = os.path.join(output_directory, f'image_{promptNumber}.png')
+
+        with open(image_path, 'wb') as image_file:
+            image_file.write(image_data)
+
+        return response
+    
+    except Exception as e:
+        # If an error occurs, return an error message or handle it as needed
+        return {"error": str(e)}
+
+
+print(create_img("someone killing someone else, with a lot of blood"))
--- code/prompt_gen.py
+++ code/prompt_gen.py
@@ -1,7 +1,8 @@
 import json
-from openai import OpenAI
+import openai
+#from openai import OpenAI
 
-client = OpenAI()
+#client = OpenAI()
 
 from helpers import apology
 
@@ -38,3 +39,44 @@ def create_prompts(blocks):
 	
 
 	return prompts
+
+def make_prompts(blocks):
+
+	prompt_count = 0
+
+	f = open('secrets.json')
+	data = json.load(f)
+	key = data["gpt"]
+	openai.api_key = key
+
+	prompts = []
+
+	messages = [ {"role": "system", "content": 
+				"Your goal is to describe the following paragraph such that a painter can make an image in its likness. You must use descriptive words. Make sure that the wording of the prompt is not violent, or rude, or sinister. The wording of the prompt must adhere to the OpenAI Dalle2 input prompt regulations. Your response must be less than 1 sentences in length. Here is the paragraph: "} ] 
+
+	while True: 
+		print("msg 1")
+
+		messages.append( 
+			{"role": "user", "content": blocks[0]}, 
+		) 
+		chat = openai.ChatCompletion.create( 
+			model="gpt-3.5-turbo", messages=messages 
+		) 
+		reply = chat.choices[0].message.content 
+		prompt_count+=1
+		prompts.append(str(prompt_count) + reply)
+		messages.append({"role": "assistant", "content": reply}) 
+		
+		if len(blocks) <  2:
+			break
+
+		blocks.pop(0)
+		print("Prompt " + str(prompt_count) + " done!")
+	
+
+	return prompts
+
+b = ['a tree', 'a fish']
+
+print(make_prompts(b))
