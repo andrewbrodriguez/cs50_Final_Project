4d751b3dbb8ef47c65c7377a9050ae6f8329cd72
bford-28 <bford1328@gmail.com>
2023-12-04 19:34:14 -0500

    bens changes

---
+++ code/templates/loading.js
@@ -0,0 +1,26 @@
+// Duck told me this was a good idea to handle errors
+'use strict'; 
+var loadingDiv = document.querySelector('.loading');
+var progressBar = document.getElementById('progress-bar');
+document.querySelector('form').addEventListener('submit', function(event)
+{
+    event.preventDefault();
+    // Show the loading screen
+    loadingDiv.style.display = 'block';
+    // Update the progress bar
+    var width = 0;
+    var interval = setInterval(function() 
+    {
+        if (width >= 100) 
+        {
+            clearInterval(interval);
+            // Hide the loading screen
+            loadingDiv.style.display = 'none';
+        } 
+        else 
+        {
+            width++; 
+            progressBar.style.width = width + '%'; 
+        }
+    }, 100);
+});
