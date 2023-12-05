9c4d845ee966a94da65a5422cf7294f88401fd30
bford-28 <bford1328@gmail.com>
2023-12-04 22:09:29 -0500

    RIIIIICK

--- code/templates/loading.js
+++ code/templates/loading.js
@@ -1,6 +1,7 @@
 // use this to prevent errors
 'use strict';
 document.querySelector('form').addEventListener('submit',function(event){
-    // Make sure rick doesn't appear prematurely
+    // Make sure form doesn't submit before rick can join us
+    event.preventDefault();
     document.querySelector('.loading').style.display = 'block';
 });
