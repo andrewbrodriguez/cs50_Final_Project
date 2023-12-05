cbd3f536d46713c1b7d68d288a8eb3735b169a39
bford-28 <bford1328@gmail.com>
2023-12-04 22:02:26 -0500

    Pushin Positivity

--- code/templates/loading.js
+++ code/templates/loading.js
@@ -1,27 +1,6 @@
+// use this to prevent errors
 'use strict';
-var loadingDiv = document.querySelector('.loading');
-var progressBar = document.getElementById('progress-bar');
-document.querySelector('form').addEventListener('submit', function(event)
-{
-    event.preventDefault();
-    // Show the loading screen
-    document.querySelector('#rick').style.display = 'block';
-    document.querySelector('#progress-container').style.display = 'block';
-    document.querySelector('#progress-bar').style.display = 'block';
-    // Update the progress bar
-    var width = 0;
-    var interval = setInterval(function()
-    {
-        if (width >= 100)
-        {
-            clearInterval(interval);
-            // Hide the loading screen
-            loadingDiv.style.display = 'none';
-        }
-        else
-        {
-            width++;
-            progressBar.style.width = width + '%';
-        }
-    }, 100);
+document.querySelector('form').addEventListener('submit',function(event){
+    // Make sure rick doesn't appear prematurely
+    document.querySelector('.loading').style.display = 'block';
 });
