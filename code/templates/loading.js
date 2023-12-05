5437e0a1dfbeb5a13e5deca1923c5016ba33e1b9
bford-28 <bford1328@gmail.com>
2023-12-04 19:55:35 -0500

    Fixing Rick

--- code/templates/loading.js
+++ code/templates/loading.js
@@ -6,7 +6,9 @@ document.querySelector('form').addEventListener('submit', function(event)
 {
     event.preventDefault();
     // Show the loading screen
-    loadingDiv.style.display = 'block';
+    document.querySelector('.loading img').style.display = 'block';
+    document.querySelector('.loading #progress-container').style.display = 'block';
+    document.querySelector('.loading #progress-bar').style.display = 'block';
     // Update the progress bar
     var width = 0;
     var interval = setInterval(function() 
