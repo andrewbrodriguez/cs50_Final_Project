cb094b4e554aa0edf28874f8842ef05d24810272
bford-28 <bford1328@gmail.com>
2023-12-04 22:38:54 -0500

    Pushin P

--- code/templates/loading.js
+++
@@ -1,7 +0,0 @@
-// use this to prevent errors
-'use strict';
-document.querySelector('form').addEventListener('submit',function(event){
-    // Make sure form doesn't submit before rick can join us
-    event.preventDefault();
-    document.querySelector('.loading').style.display = 'block';
-});
